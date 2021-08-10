82338af5acb4301346f8c124232e158c2afcfb91
stasia <stasia@soloduha.com>
2021-08-10 17:08:52 +0300

    did second try to create tasks

--- discount_spy/tasks/models.py
+++ discount_spy/tasks/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Task(models.Model):
     name = models.CharField(max_length=30)
-    target_price = models.DecimalField(decimal_places=2, max_digits=10)
+    target_price = models.DecimalField(decimal_places=2, max_digits=10)  # ожидаемая цена
     email = models.EmailField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    url = models.URLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    url = models.URLField()     # ссылка на товар
--- discount_spy/tasks/views.py
+++ discount_spy/tasks/views.py
@@ -2,15 +2,25 @@ import json
 
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Task
 
 
 # Create your views here.
+@csrf_exempt
 def add_task(request):
-    if request.is_ajax():
-        if request.method == 'POST':
-            data = json.loads(request.body)
-
-    new_task = Task()
+    data = None
+    # if request.is_ajax():
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        new_task = Task(
+            name=data['name'],
+            target_price=data['target_price'],
+            email=data['email'],
+            url=data['url']
+        )
+        new_task.save()
+    if data is None:
+        return JsonResponse({"error": "We don't have data!"})
     return JsonResponse({"message": "Task added!", "status": "ok"})
