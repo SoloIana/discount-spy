2f4accf8783ef3b7ef2541aa90eec9f807c769b0
stasia <stasia@soloduha.com>
2021-08-10 16:22:29 +0300

    did first try to create tasks

--- discount_spy/tasks/models.py
+++ discount_spy/tasks/models.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
-
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=30)
+    target_price = models.DecimalField(decimal_places=2, max_digits=10)
+    email = models.EmailField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    url = models.URLField()
--- discount_spy/tasks/views.py
+++ discount_spy/tasks/views.py
@@ -1,3 +1,16 @@
+import json
+
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from .models import Task
+
 
 # Create your views here.
+def add_task(request):
+    if request.is_ajax():
+        if request.method == 'POST':
+            data = json.loads(request.body)
+
+    new_task = Task()
+    return JsonResponse({"message": "Task added!", "status": "ok"})
